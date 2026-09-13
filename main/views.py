18f0b6c6a18ac1f083ca356dbd33b4ec104295f8
malvlionn <malvinlionard@gmail.com>
2026-09-13 20:49:47 +0700

    feat(views): implement show_awards view and configure url routing

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Award
 
 
 def show_main(request):
@@ -22,3 +22,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_awards(request):
+    awards = Award.objects.all()
+    context = {
+        "name": "Malvin Lionard",
+        "award_list": awards,
+    }
+    return render(request, "awards.html", context)
